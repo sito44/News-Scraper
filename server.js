17bb80f210d15d2b4f194cd8910e81dc7a852cc7
MacbookPro <andreesito@gmail.com>
2018-03-20 00:06:27 -0700

    headline model, controller and api route created - server and index files updated

--- server.js
+++ server.js
@@ -6,25 +6,29 @@ const cheerio = require('cheerio');
 const exphbs = require('express-handlebars');
 
 const db = require('./models');
-
-/* const apiRoutes = require('./routes/api/api-routes.js');
-const htmlRoutes = require('./routes/view/html-routes.js'); */
+const PORT = 3030;
+const apiRoutes = require('./routes/api/api-routes.js');
+/*const htmlRoutes = require('./routes/view/html-routes.js'); */
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
 
-/* app.use(apiRoutes);
-app.use(htmlRoutes); */
+app.use(apiRoutes);
+/*app.use(htmlRoutes); */
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-    useMongoClient: true
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
+
