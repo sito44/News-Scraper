0d53e2b812ee22fb51a4a5439e98df78f8705670
MacbookPro <andreesito@gmail.com>
2018-03-23 20:43:04 -0700

    scrape button added to iu with functionality - css edits

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -21,10 +21,21 @@ $(function(){
         const articleId = {
             id: $(this).attr('data-acticle-id')
         };
-        console.log(articleId.id);
+        
         $.ajax('/api/delete-article', {
             type: 'PUT',
             data: articleId
+        }).then(function (result) {
+            console.log(result + 'updated!');
+            location.reload();
+        });
+    });
+
+    $(document).on('click', '.btn-success', function (e) {
+        e.preventDefault();
+        
+        $.ajax('/api/initial-scrape', {
+            type: 'GET',
         }).then(function (result) {
             console.log(result + 'updated!');
             location.reload();
