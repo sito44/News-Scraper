e94e56d76e0ce1634658d81d99be1fd71fb3dea4
Andre Holguin <andreesito@gmail.com>
2019-07-27 15:16:53 -0700

    whoops forgot to delete old commented out code commit :|

--- models/Headline.js
+++ models/Headline.js
@@ -57,62 +57,6 @@ headlineSchema.statics.scrape = function (callback) {
     })
     .catch(err => console.log(err));
 
-
-
-
-
-
-
-
-    // const hostName = "https://www.naturalnews.com/";
-
-    // let initialScrapeArray = []
-
-    // request(hostName, function (error, response, html) {
-
-    //     if (error) console.log(error);
-
-    //     const $ = cheerio.load(html);
-
-    //     $("div.IFB").each(function (i, element) {
-    //         // Save the text and href of each link enclosed in the current element
-    //         let date = $(element).children("div.IFA").children("time.entry-time").text();
-    //         let title = $(element).children("div.IFH").children("a").text();
-    //         let author = $(element).children("div.IFA").children("span.entry-author").children("a").children().text();
-    //         let imagePath = $(element).children("div.IFP").children("a").children().attr("src");
-    //         let description = $(element).children("div.IFD").find("p").text();
-    //         let link = $(element).children("div.IFH").children("a").attr("href");
-    //         let imageUrl = hostName + imagePath;
-
-    //         let articleData = {
-    //             date: date,
-    //             title: title,
-    //             author: author,
-    //             imageUrl: imageUrl,
-    //             description: description,
-    //             link: link
-    //         };
-
-
-    //         if (date && title && author && imageUrl && description && link) {
-
-    //             initialScrapeArray.push(articleData);
-
-    //             // Headline.create(articleData, function (err, inserted) {
-    //             //     if (err) {
-
-    //             //         console.log(err);
-    //             //     } else {
-
-    //             //         console.log(inserted + 'HERE --------------------*************');
-    //             //     }
-    //             // });
-    //         }
-    //     });
-    //     callback(initialScrapeArray);
-
-    // });
-
 };
 
 headlineSchema.statics.dbQuery = function (callback) {
