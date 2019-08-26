3589b9906588f96374929c0b84a0097eaaa0d821
Andre Holguin <andreesito@gmail.com>
2019-08-26 16:18:06 -0700

    scrape template bug fixed

--- scrapeTemplates/snlsNetwork.js
+++ scrapeTemplates/snlsNetwork.js
@@ -12,60 +12,81 @@ module.exports = async function () {
 
 
 
-                let title1 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").children("h6").children("a").text();
+                let title1 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").children().eq(0).children("a").text();
                 let link1 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").children("h6").children("a").attr("href");
-                let date1 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").children("span").text();
+                let date1 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").children().eq(1).text();
 
-                let title2 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().children("h6").children("a").text();
+                let title2 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().children().eq(0).children("a").text();
                 let link2 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().children("h6").children("a").attr("href");
-                let date2 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().children("span").text();
+                let date2 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().children().eq(1).text();
 
-                let title3 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().children("h6").children("a").text();
+                let title3 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().children().eq(0).children("a").text();
                 let link3 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().children("h6").children("a").attr("href");
-                let date3 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().children("span").text();
+                let date3 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().children().eq(1).text();
 
-                let title4 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().children("h6").children("a").text();
+                let title4 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().children().eq(0).children("a").text();
                 let link4 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().children("h6").children("a").attr("href");
-                let date4 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().children("span").text();
+                let date4 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().children().eq(1).text();
 
-                let title5 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().children("h6").children("a").text();
+                let title5 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().children().eq(0).children("a").text();
                 let link5 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().children("h6").children("a").attr("href");
-                let date5 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().children("span").text();
+                let date5 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().children().eq(1).text();
 
-                let title6 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().next().children("h6").children("a").text();
+                let title6 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().next().children().eq(0).children("a").text();
                 let link6 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().next().children("h6").children("a").attr("href");
-                let date6 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().next().children("span").text();
+                let date6 = $(element).children("div.qt-part-archive-item.qt-item-inline.qt-clearfix").next().next().next().next().next().children().eq(1).text();
                 
 
                 let linkData = {
                     domain: hostName,
-                    date1: date1.slice(0,24).trim(),
+                    date1: date1,
                     title1: title1,
                     link1: link1,
-                    date2: date2.slice(0, 24).trim(),
+                    date2: date2, 
                     title2: title2,
                     link2: link2,
-                    date3: date3.slice(0, 24).trim(),
+                    date3: date3, 
                     title3: title3,
                     link3: link3,
-                    date4: date4.slice(0, 24).trim(),
+                    date4: date4, 
                     title4: title4,
                     link4: link4,
-                    date5: date5.slice(0, 24).trim(),
+                    date5: date5, 
                     title5: title5,
                     link5: link5,
-                    date6: date6.slice(0, 24).trim(),
+                    date6: date6, 
                     title6: title6,
                     link6: link6
                 };
 
 
-                
-
+                if (
+                    date1 &&
+                    title1 &&
+                    link1 &&
+                    date2 &&
+                    title2 &&
+                    link2 &&
+                    date3 &&
+                    title3 &&
+                    link3 &&
+                    date4 &&
+                    title4 &&
+                    link4 &&
+                    date5 &&
+                    title5 &&
+                    link5 &&
+                    date6 &&
+                    title6 &&
+                    link6
+                ) {
+                    
                     data.push(linkData);
+                }
+
                 
             });
-            console.log(data);
+            
             return data;
 
         })
